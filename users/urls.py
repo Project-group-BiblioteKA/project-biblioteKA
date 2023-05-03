9347aca298ab0732c2b0f62f301373fff9caf8e7
Dionisio <benevidescursos@gmail.com>
2023-05-03 09:51:09 -0300

    feat: creating urls and permissions

--- users/urls.py
+++ users/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 from . import views
 from rest_framework_simplejwt import views as jwt_views
 
-urlpatterns = []
+urlpatterns = [path("loand/<int:user_id>", views.LoanView.as_view())]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import generics
+
+from copies.models import LoandBook
 
 # Create your views here.
+
+
+class LoanView(generics.ListCreateAPIView):
+    queryset = LoandBook.objects.all()
